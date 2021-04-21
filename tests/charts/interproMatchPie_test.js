3963770627dfc41e5bf317f8e32b239ab4c23466
Gustavo A. Salazar <gustavoadolfo.salazar@gmail.com>
2021-04-21 16:48:35 +0100

    github actions: attempt 45

--- tests/charts/interproMatchPie_test.js
+++ tests/charts/interproMatchPie_test.js
@@ -1,31 +1,33 @@
-define(['charts/interproMatchPie'], function(InterproMatchPie) {
-    const apiConfig = {
-        API_URL: window.__env__['API_URL'],
-        SUBFOLDER: '/metagenomics'
-    };
-    const containerID = 'chart-container';
+define(["charts/interproMatchPie"], function (InterproMatchPie) {
+  const apiConfig = {
+    API_URL: window.__env__["API_URL"],
+    SUBFOLDER: "/metagenomics",
+  };
+  const containerID = "chart-container";
 
-    /**
-     *
-     */
-    function createDiv() {
-        document.body.innerHTML = '<p></p>';
-        document.body.innerHTML = ('<div id="' + containerID + '"></div>');
-    }
+  /**
+   *
+   */
+  function createDiv() {
+    document.body.innerHTML = "<p></p>";
+    document.body.innerHTML = '<div id="' + containerID + '"></div>';
+  }
 
-    describe('Interpro match pie chart', function() {
-        context('Data loading source', function() {
-            it('Should fetch data from accession', function(done) {
-                this.timeout(20000);
-                createDiv();
-                const accession = 'MGYA00141547';
-                const chart = new InterproMatchPie(containerID,
-                    {accession: accession, apiConfig: apiConfig});
-                chart.loaded.done(() => {
-                    expect($('.highcharts-point').length).to.equal(11);
-                    done();
-                });
-            });
+  describe("Interpro match pie chart", function () {
+    context("Data loading source", function () {
+      it("Should fetch data from accession", function (done) {
+        this.timeout(50000);
+        createDiv();
+        const accession = "MGYA00141547";
+        const chart = new InterproMatchPie(containerID, {
+          accession: accession,
+          apiConfig: apiConfig,
         });
+        chart.loaded.done(() => {
+          expect($(".highcharts-point").length).to.equal(11);
+          done();
+        });
+      });
     });
+  });
 });
